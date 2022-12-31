9fdc463346759157a18d772d72233f6b34f0d1d4
imentu <imentu@foxmail.com>
2022-12-31 21:26:57 +0800

    last commit for 2022

--- src/components/vitepress/ProjectProgresses.vue
+++ src/components/vitepress/ProjectProgresses.vue
@@ -4,37 +4,37 @@ import ProjectCard from '../project/ProjectCard.vue'
 </script>
 
 <template>
-  <h2>Doing</h2>
-  <div
-    v-for="project in projects.data.doing"
-    :key="project.title"
-  >
-    <ProjectCard
-      :project="project"
-    />
+  <div v-if="projects.data.doing.length > 0">
+    <h2>Doing</h2>
+    <div
+      v-for="project in projects.data.doing"
+      :key="project.title"
+    >
+      <ProjectCard :project="project" />
+    </div>
   </div>
-  <h2>Todo</h2>
-  <div
-    v-for="project in projects.data.todo"
-    :key="project.title"
-  >
-    <ProjectCard
-      :project="project"
-    />
+  <div v-if="projects.data.todo.length > 0">
+    <h2>Todo</h2>
+    <div
+      v-for="project in projects.data.todo"
+      :key="project.title"
+    >
+      <ProjectCard :project="project" />
+    </div>
   </div>
-  <h2>Done</h2>
-  <div
-    v-for="project in projects.data.done"
-    :key="project.title"
-  >
-    <ProjectCard
-      :project="project"
-    />
+  <div v-if="projects.data.done.length > 0">
+    <h2>Done</h2>
+    <div
+      v-for="project in projects.data.done"
+      :key="project.title"
+    >
+      <ProjectCard :project="project" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-div{
+div {
   @apply my-8
 }
 </style>
